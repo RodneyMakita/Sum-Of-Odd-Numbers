d4181a714878064295eb1c098130658405197664
RodneyMakita <sibongisenitimba@gmail.com>
2024-04-10 12:06:16 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+def row_sum_odd_numbers(n):
+    first_number = (n - 1) * n + 1
+    return n * n * n
+
+print(row_sum_odd_numbers(0))   
+print(row_sum_odd_numbers(444))  
+print(row_sum_odd_numbers(990)) 
+print(row_sum_odd_numbers(4840)) 
